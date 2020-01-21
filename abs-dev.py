5dcb45489f77d63eff5aca3b0de76edd69055028
bobby233 <mczsjzsjz@icloud.com>
2020-01-21 17:52:40 +0800

    add dev version

---
+++ abs-dev.py
@@ -0,0 +1,9 @@
+#Created by bobby233.Github:https://github.com/bobby233/learnpython
+#Version:V1.0.1d
+a = input('type a number and change it into abs:')
+b = int(a)
+
+if b >= 0:
+    print(b)
+else:
+    print(-b)
